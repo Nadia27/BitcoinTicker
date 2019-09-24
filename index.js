3a452608def46d92778cbdb2552573e50a541ea7
Nadia27 <jones.nadia.l@gmail.com>
2019-09-24 11:32:33 -0400

    finished tracker

--- index.js
+++ index.js
@@ -2,17 +2,28 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function (req, res) {
+    //console.log(req.body.crypto);
+    request('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD', function (err, res, body) {
+    var data = JSON.parse(body); // convert JSON data in JS obj
+    console.log(data);
+    var price = data.last;
+    console.log(price);
+    });
+
+});
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
